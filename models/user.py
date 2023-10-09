1bccaf936d27b145bcdf45cd6aae211201ebad8d
Shaheer <shaheer.mirxa@hotmail.com>
2023-10-10 01:44:12 +0500

    CodeReviewed

--- models/user.py
+++ models/user.py
@@ -42,7 +42,7 @@ class Role(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return '<Role \'%s\'>' % self.name
+        return f'<Role \'{self.name}\'>'
 
 
 class User(UserMixin, db.Model):
@@ -56,7 +56,7 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         if self.role is None:
             if self.email == current_app.config['ADMIN_EMAIL']:
                 self.role = Role.query.filter_by(
@@ -65,7 +65,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
 
     def full_name(self):
-        return '%s %s' % (self.first_name, self.last_name)
+        return f'{self.first_name} {self.last_name}'
 
     def can(self, permissions):
         return self.role is not None and \
@@ -177,7 +177,7 @@ class User(UserMixin, db.Model):
                 db.session.rollback()
 
     def __repr__(self):
-        return '<User \'%s\'>' % self.full_name()
+        return f'<User \'{self.full_name()}\'>'
 
 
 class AnonymousUser(AnonymousUserMixin):
